340da4f8f9cf5bee374eac2b6d474cfcbfe79329
jcfdelpino <juan.delpino@kellogg.northwestern.edu>
2022-02-07 02:04:53 +0000

    2

--- kmdb.rb
+++ kmdb.rb
@@ -69,12 +69,18 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
-People.destroy_all
-Movie.destroy_all
-Role.destroy_all
+
 # Generate models and tables, according to the domain model
 # TODO!
-class 
+ batman_begins = Movie.new
+ id = "1"
+ title = "Batman Begins" 
+ year_released = "2005"
+ rating = "PG-13"
+ puts batman_begins.inspect
+
+
+
 
 # Insert data into your database that reflects the sample data shown above
 # Do not use hard-coded foreign key IDs.
